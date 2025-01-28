e0de39b636b54d238799176e1a12fdce383cfcd2
zzdpk2 <zzdpk2@126.com>
2025-01-29 02:38:39 +1100

    Finish test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,8 +3,25 @@
 namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, Alex!");
+
+            int number1 = 0;
+            Console.WriteLine("number1: ", number1);
+
+            Console.WriteLine("My name is {0}. I am {1} year old.", "Alex", 100);
+
+            // Embedded String
+            string name = "Alex";
+            int age = 101;
+            string message3 = $"My name is {name}. I am {age} year old.";
+            Console.WriteLine(message3);
+
+            Console.WriteLine(@"My name is {0}.\n
+I am {1}
+
+
+
+year old.\n", 
+"Alex", 100);
 
             Console.Read();
         }
