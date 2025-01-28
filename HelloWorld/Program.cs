97a9ad21e97e092a42ea647d1785eb8c9c773d2b
zzdpk2 <zzdpk2@126.com>
2025-01-28 16:43:19 +1100

    System.Console Print

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
+﻿using System;
 
 namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
-             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Alex!");
+
+            Console.Read();
         }
     }
 }
